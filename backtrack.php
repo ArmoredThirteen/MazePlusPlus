50f2b8a253228a31ab4999acf1c9bf8cf699c5db
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2020-11-10 11:15:16 -0800

    With continued thanks to Corsair:  Fixed startingX/Y when opening the page, fixed startingX/Y not lowering correctly, fixed error message showing when it shouldn't, fixed bug where reloading page allowed startingX/Y could be incremented too far, fixed visual page width bug

--- backtrack.php
+++ backtrack.php
@@ -5,8 +5,9 @@ $rows = $_POST['rows'] ?? 10;
 $cols = $_POST['cols'] ?? 10;
 $seed = $_POST['seed'] ?? 0;
 
-$startX = $_POST['startX'] ?? 0;
-$startY = $_POST['startY'] ?? 0;
+$start
+ = $_POST['startX'] ?? $rows - 1;
+$startY = $_POST['startY'] ?? $cols - 1;
 
 $wOne = $_POST['wOne'] ?? 1;
 $wTwo = $_POST['wTwo'] ?? 1;
@@ -27,7 +28,7 @@ $escWFour = escapeshellarg($wFour);
 
 
 // seed, xLen, yLen, startX, startY, direction weights 1-4
-exec("./MazeGen.out backtrack $escSeed $escCols $escRows $escStartX $escStartY $escWOne $escWTwo $escWThree $escWFour", $out, $return);
+shell_exec("./MazeGen.out backtrack $escSeed $escCols $escRows $escStartX $escStartY $escWOne $escWTwo $escWThree $escWFour", $out, $return);
 
 if ($return) {
     $response = "Generation failed. :(";
@@ -39,16 +40,11 @@ else {
     // Pass maze data to image generation page and display returned image
     $image = "<img src='mazeImage.php?rows=$rows&cols=$cols&maze=$out[0]' alt='Generated Maze' />";
 }
-
-// Print entire output text
-/*foreach ($out as $val) {
-    echo "$val\r\n";
-}*/
 ?>
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Backtrack</title>
+    <title>Insert Title Here</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -60,71 +56,79 @@ else {
     <div class="container mt-4">
         <div class="row justify-content-center">
             <div class="col-sm-12 col-md-9">
-                <?php if(isset($image)) echo $image; ?>
-                <?php if(isset($response))?>
-                <!--<div class="alert alert-danger">
+                <div class="text-center">
+                    <?php if(isset($image)) echo $image; ?>
+                </div>
+                <?php if(isset($response)) { ?>
+                <div class="alert alert-danger">
                     <b>An error occured:</b> <?php echo $response; ?><br/>
                     <?php print_r($debug); ?>
-                </div>-->
+                </div>
+                <?php } ?>
 
                 <form method="post" action="backtrack.php">
-                    <h3 class="font-weight-bold">Basic Settings</h3>
-                    <div class="form-row">
-                        <div class="form-group col-md-3">
-                            Rows (5-25):
-                            <input type="number" class="form-control" name="rows" id="rows" min="5" max="25"
-                                   value="<?php echo $rows; ?>" />
+                    <div class="card card-body shadow-lg mt-4">
+                        <h3 class="font-weight-bold">Basic Settings</h3>
+                        <div class="form-row">
+                            <div class="form-group col-md-3">
+                                Rows (5-25):
+                                <input type="number" class="form-control" name="rows" id="rows" min="5" max="25"
+                                    value="<?php echo $rows; ?>" />
+                            </div>
+                            <div class="form-group col-md-3">
+                                Cols (5-25):
+                                <input type="number" class="form-control" name="cols" id="cols" min="5" max="25"
+                                    value="<?php echo $cols; ?>" />
+                            </div>
+                            <div class="form-group col-md-3">
+                                Starting X:
+                                <input type="number" class="form-control"  name="startX" id="x" min="0"
+                                    max="" value="0" />
+                            </div>
+                            <div class="form-group col-md-3">
+                                Starting Y:
+                                <input type="number" class="form-control"  name="startY" id="y" min="0"
+                                    max="" value="0" />
+                            </div>
                         </div>
-                        <div class="form-group col-md-3">
-                            Cols (5-25):
-                            <input type="number" class="form-control" name="cols" id="cols" min="5" max="25"
-                                   value="<?php echo $cols; ?>" />
-                        </div>
-                        <div class="form-group col-md-3">
-                            Seed (0 for random):
-                            <input type="number" class="form-control"  name="seed" min="0"
-                                    value="<?php echo $seed; ?>" />
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-3">
-                            Starting X:
-                            <input type="number" class="form-control"  name="startX" id="x" min="0"
-                                   value="<?php echo $rows - 1; ?>" />
-                        </div>
-                        <div class="form-group col-md-3">
-                            Starting Y:
-                            <input type="number" class="form-control"  name="startY" id="y" min="0"
-                                   value="<?php echo $cols - 1; ?>" />
-                        </div>
-                    </div>
-                    <hr>
 
-                    <h3 class="font-weight-bold">Movement Direction Weights</h3>
-                    <div class="form-row">
-                        <div class="form-group col-md-3">
-                            Left (1-20):
-                            <input type="number" class="form-control"  name="wOne" min="1" max="20"
-                                   value="<?php echo $wOne; ?>" />
-                        </div>
-                        <div class="form-group col-md-3">
-                            Right (1-20):
-                            <input type="number" class="form-control"  name="wTwo" min="1" max="20"
-                                   value="<?php echo $wTwo; ?>" />
-                        </div>
-                        <div class="form-group col-md-3">
-                            Up (1-20):
-                            <input type="number" class="form-control"  name="wThree" min="1" max="20"
-                                   value="<?php echo $wThree; ?>" />
-                        </div>
-                        <div class="form-group col-md-3">
-                            Down (1-20):
-                            <input type="number" class="form-control"  name="wFour" min="1" max="20"
-                                   value="<?php echo $wFour; ?>" />
+                        <div class="form-row">
+                        <div class="form-group col-md-6 mb-0">
+                                Seed:
+                                <input type="number" class="form-control"  name="seed" min="0"
+                                        value="<?php echo $seed; ?>" />
+                            </div>
                         </div>
                     </div>
+                    <div class="card card-body shadow-lg mt-4">
+                        <h3 class="font-weight-bold">Movement Direction Weights</h3>
+                        <div class="form-row">
+                            <div class="form-group col-md-3 mt-2 mb-0">
+                                Left (1-20):
+                                <input type="number" class="form-control"  name="wOne" min="1" max="20"
+                                    value="<?php echo $wOne; ?>" />
+                            </div>
+                            <div class="form-group col-md-3 mt-2 mb-0">
+                                Right (1-20):
+                                <input type="number" class="form-control"  name="wTwo" min="1" max="20"
+                                    value="<?php echo $wTwo; ?>" />
+                            </div>
+                            <div class="form-group col-md-3 mt-2 mb-0">
+                                Up (1-20):
+                                <input type="number" class="form-control"  name="wThree" min="1" max="20"
+                                    value="<?php echo $wThree; ?>" />
+                            </div>
+                            <div class="form-group col-md-3 mt-2 mb-0">
+                                Down (1-20):
+                                <input type="number" class="form-control"  name="wFour" min="1" max="20"
+                                    value="<?php echo $wFour; ?>" />
+                            </div>
+                        </div>
+                    </div>
+                    <p class="text-center mt-4">
+                        <input class="btn btn-lg btn-success" type="submit" value="Generate Maze"/>
 
-                    <input class="btn btn-success" type="submit" value="Generate"/>
+                    </p>
                 </form>
             </div>
         </div>
@@ -137,19 +141,25 @@ else {
         x = $('#x');
         y = $('#y');
 
+        // Let's set the max starting points right away.
+        x.attr('max', rows.val() - 1);
+        y.attr('max', cols.val() - 1);
+
+        // If the values of Rows or Columns is changed,
+        // update the max + val for the starting x + y.
         rows.on('input', function() {
             x.attr({
                 max: rows.val() - 1,
                 value: rows.val() - 1
             });
-            x.val(rows.val());
+            x.val(rows.val() - 1);
         });
         cols.on('input', function() {
             y.attr({
                 max: cols.val() - 1,
                 value: cols.val() - 1
             });
-            y.val(cols.val());
+            y.val(cols.val() - 1);
         });
     </script>
 </body>
